d442f6e8585144e4ca084955f6522ac52e7a8655
Sebastian Häni <haeni.sebastian@gmail.com>
2016-06-07 20:25:02 +0200

    first sample graph

--- app/containers/ModuleDetail/index.js
+++ app/containers/ModuleDetail/index.js
@@ -12,8 +12,8 @@ import selectModuleDetail from './selectors';
 import styles from './styles.css';
 
 export class ModuleDetail extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    console.log(this.props.params.id);
+
+  componentDidMount() {
     // create an array with nodes
     var nodes = new vis.DataSet([
       {id: 1, label: 'Node 1'},
@@ -22,7 +22,6 @@ export class ModuleDetail extends React.Component { // eslint-disable-line react
       {id: 4, label: 'Node 4'},
       {id: 5, label: 'Node 5'}
     ]);
-
     // create an array with edges
     var edges = new vis.DataSet([
       {from: 1, to: 3},
@@ -32,18 +31,28 @@ export class ModuleDetail extends React.Component { // eslint-disable-line react
     ]);
 
     // create a network
-    var container = document.getElementById('mynetwork');
     var data = {
       nodes: nodes,
       edges: edges
     };
     var options = {};
-    var network = new vis.Network(container, data, options);
+    console.log(this.canvas);
+    var network = new vis.Network(this.canvas, data, options);
+  }
+
+  render() {
+    console.log(this.props.params.id);
+
+    let canvas = {
+      width: '100%',
+      height: '500px'
+    };
 
     return (
       <div className={ styles.moduleDetail }>
         <H1>{this.props.params.id}</H1>
         This is ModuleDetail container !
+        <div style={canvas} ref={(ref) => this.canvas = ref}></div>
       </div>
     );
   }
